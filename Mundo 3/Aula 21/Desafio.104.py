861b617941d17d26da8402540da18c34eb841d2c
Hugo Silva <developer.hugosilva@gmail.com>
2026-01-24 19:28:18 -0300

    Commit
    Resolução do 104 - Validar na aula de explicação

--- Mundo 3/Aula 21/Desafio.104.py
+++ Mundo 3/Aula 21/Desafio.104.py
@@ -1,16 +1,30 @@
 def leiaint(n):
 
-    n = int(n)
-    print(type(n))
+    #print(type(n))
+    i = 0
+    try:
+        int(n)
+        i = 1
+        return n,i
+    except ValueError:
+        print('DIGITE UM NÚMERO INTEIRO.')
+        i = 0
+        return n, i
+        #return False
 
+'''
     if type(n) == int:
         return n
     else:
         print('DIGITE UM NÚMERO INTEIRO.')
+'''
 
 #Programa Principal
 valor = input('Digite um valor inteiro: ')
 n = leiaint(valor)
-print(f'Você acabou de digitar o número {n}')
 
-#Ver resolução dessa
+#print(n[1])
+
+if n[1] == 1:
+    print(f'Você acabou de digitar o número {n[0]}')
+
