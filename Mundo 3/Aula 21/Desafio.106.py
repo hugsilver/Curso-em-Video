76d5341912881d6f53029bde4173a9091111a5f8
Hugo Silva <developer.hugosilva@gmail.com>
2026-01-24 12:46:16 -0300

    Commit
    Atualização do desafio 106
    #Falta validar para que a ultima função ou biblioteca seja limpa, se der enter ele puxa a ultima

--- Mundo 3/Aula 21/Desafio.106.py
+++ Mundo 3/Aula 21/Desafio.106.py
@@ -4,14 +4,13 @@ def pyhelp(fun):
     modtex2(fun)
     return fun
 
-def modtex(a):
+def modtex(a): #Função para colorir o 1° Texto
     b = ('~'*(len(a)))
     print(f"\033[37;42m{b}\033[0m")
     print(f"\033[37;42m{a}\033[0m")
     print(f"\033[37;42m{b}\033[0m")
 
-def modtex2(fun):
-
+def modtex2(fun): #Função para colorir o Texto da função
     c = ('~'*(30 + len(fun)))
     d = (f'Acessando o manual do comando {fun}')
     print(f"\033[37;44m{c}\033[0m")
@@ -19,11 +18,21 @@ def modtex2(fun):
     print(f"\033[37;44m{c}\033[0m")
     #e = str(help(fun))
     print(f"\033[30;47m\033[1m")
-    print(f"\033[30;47m{help(fun)}\033[0m")
+    fun = print(f"\033[30;47m{help(fun)}\033[0m")
+    return fun
 
 
-modtex('SISTEMA DE AJUDA PyHELP')
+while True:
 
-fun = pyhelp(str(input('Função ou Biblioteca >')))
+    x = 0
+    modtex('SISTEMA DE AJUDA PyHELP')
+    x = str(input('Função ou Biblioteca >'))
+    if x == 'EXIT':
+        break
 
-#Falta colcoar em laço de repetição While = True p/ varias funções ou bibliotecas e voltar pro menu (Pro painel), quando digitar o fim, dar mensagem e finalizar
+    fun = pyhelp(x)
+    print(f"\033[30;47m\033[1m")
+    print(fun)
+
+
+#Falta retornar pro menu principal
