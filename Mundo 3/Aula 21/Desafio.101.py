3f07091674f2b3e853a67524aed3218387f0bea2
Hugo Silva <developer.hugosilva@gmail.com>
2026-01-26 23:48:33 -0300

    Commit de Atualização
    Exercicios 101 e 102 - Metodo Guana

--- Mundo 3/Aula 21/Desafio.101.py
+++ Mundo 3/Aula 21/Desafio.101.py
@@ -1,15 +1,12 @@
+'''
+
 from datetime import date
 
 id = 0
 status = 0
 
 def valida_id(id):
-    '''
-    Docstring para valida_id
-    
-    :param id: Recebe a idade da pessoa
 
-    '''
     if id >=18 and id < 70:
         status = 'OBRIGATORIO'
         return status 
@@ -24,14 +21,31 @@ def valida_id(id):
         return status 
     
 
-
+#Programa Principal
 year_nascimento = int(input('Digite o ano em que você nasceu: '))
 hoje = date.today()
 id = hoje.year - year_nascimento
 result = valida_id(id)
 
 print(f'Para idade de {id} o voto é {result}')
+'''
+
+#Metodo Guanabara
+
+def voto(ano):
+    from datetime import date #Escopo de importação - Economiza memoria - Se for necessario somente dentro da função
+    atual = date.today().year
+    idade = atual - ano
+
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade <18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGÁTORIO.'
 
 
+nas = int(input('Em que ano você nasceu? '))
+print(voto(nas))
 
 
--- Mundo 3/Aula 21/Desafio.102.py
+++ Mundo 3/Aula 21/Desafio.102.py
@@ -24,7 +24,44 @@ def fatorial(num=1, show = 0):
         return f
         
 
-print(fatorial(5, show = False))
+print(fatorial(5, show = True))
+print(help(fatorial))
+
+#Metodo Guana
+
+def fatorial(n, show = False):
+    '''
+    Docstring para fatorial
+    -> Calcular o Fatorial de um número.
+    :param n: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    : return: O valor do Fatorial de um núemro n
+    '''
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        f *= c
+    return f
+
+#print(fatorial(5, show = True))
+
+help(fatorial)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
